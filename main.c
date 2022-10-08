1fb6ffbd783a840ffdb4baa09985c739b057caf3
David Laubreiter <david.laubreiter@outlook.com>
2022-10-08 21:29:41 +0200

    feat(matrix): Implementation

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
+
+#define MATRIX_SIZE 1000
+
+int32_t a[MATRIX_SIZE][MATRIX_SIZE];
+int32_t b[MATRIX_SIZE][MATRIX_SIZE];
+int64_t c[MATRIX_SIZE][MATRIX_SIZE];
+
+
+void initializeWithRandoms();
+
+void multiply();
+
+int main() {
+    srand(time(0));
+
+
+    initializeWithRandoms();
+
+    multiply();
+
+    return 0;
+}
+
+void initializeWithRandoms() {
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            a[i][j] = rand() % 10;
+            b[i][j] = rand() % 10;
+        }
+    }
+}
+
+void multiply() {
+
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            int sum = 0;
+            for (int k = 0; k < MATRIX_SIZE; k++) {
+                sum += a[i][k] * b[k][j];
+            }
+
+            c[i][j] = sum;
+        }
+    }
+}
