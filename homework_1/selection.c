6c8de6b5879bd02d04937c063c6686090f2153e3
David Laubreiter <david.laubreiter@outlook.com>
2022-10-21 15:22:28 +0200

    feat(selection): Implementation

---
+++ homework_1/selection.c
@@ -0,0 +1,43 @@
+
+#include <stdlib.h>
+#include <time.h>
+
+#ifndef ARR_LEN
+# define ARR_LEN 1000
+#endif
+
+int A[ARR_LEN];
+
+void generateRandoms() {
+    srand(time(0));
+    for(int i = 0; i < ARR_LEN; i++) {
+        A[i] = rand();
+    }
+}
+
+void selectionSort() {
+
+    for(int i = 0; i < ARR_LEN-1; i++) {
+        int jMin = i;
+        for(int j = i+1; j < ARR_LEN; j++) {
+            if (A[j] < A[jMin]) {
+                jMin = j;
+            }
+        }
+
+        if (jMin != i) {
+            int t = A[i];
+            A[i] = A[jMin];
+            A[jMin] = t;
+        }
+    }
+}
+
+
+int main() {
+    generateRandoms();
+
+    selectionSort();
+
+    return 0;
+}
