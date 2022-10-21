89c93c4c1b2e4208e1bfe0648512986fd85c32e7
David Laubreiter <david.laubreiter@outlook.com>
2022-10-21 15:34:19 +0200

    feat(quicksort): Implementation

---
+++ homework_1/quicksort.c
@@ -0,0 +1,63 @@
+
+#include <stdlib.h>
+#include <time.h>
+
+#ifndef ARR_LEN
+# define ARR_LEN 1000
+#endif
+
+int A[ARR_LEN];
+
+void generateRandoms() {
+    srand(time(0));
+    for(int i = 0; i < ARR_LEN; i++) {
+        A[i] = rand();
+    }
+}
+
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int *a, int lo, int hi) {
+    int pivot = a[hi];
+
+    int i = lo - 1;
+
+    for(int j = lo; j < hi; j++) {
+        if (a[j] <= pivot) {
+            i++;
+            swap(a + i, a + j);
+        }
+    }
+
+    i++;
+    swap(a+i, a+hi);
+
+    return i;
+}
+
+void quicksort(int*a, int lo, int hi) {
+
+    if (lo >=hi || lo < 0) {
+        return;
+    }
+
+    int p = partition(a, lo, hi);
+
+    quicksort(a, lo, p-1);
+    quicksort(a, p+1, hi);
+}
+
+
+
+
+int main() {
+    generateRandoms();
+
+    quicksort(A, 0, ARR_LEN-1);
+
+    return 0;
+}
