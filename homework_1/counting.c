dca91ef6f35119095d9251320a1bde5e234f5f66
David Laubreiter <david.laubreiter@outlook.com>
2022-10-21 15:08:53 +0200

    feat(counting): Implementation

---
+++ homework_1/counting.c
@@ -0,0 +1,46 @@
+
+#include <stdlib.h>
+#include <time.h>
+
+#ifndef ARR_LEN
+#define ARR_LEN 1000
+#endif
+
+#define MAX 100
+
+int A[ARR_LEN];
+int O[ARR_LEN];
+
+int COUNT[MAX + 1] = {0};
+
+void generateRandoms() {
+    srand(time(0));
+    for(int i = 0; i < ARR_LEN; i++) {
+        A[i] = rand() % MAX;
+    }
+}
+
+void countingSort(){
+    for(int i = 0; i < ARR_LEN; i++) {
+        COUNT[A[i]]++;
+    }
+
+    for(int i = 1; i < MAX; i++) {
+        COUNT[i] = COUNT[i] + COUNT[i-1];
+    }
+
+    for(int i = ARR_LEN -1; i >=0; i--) {
+        int j = A[i];
+        COUNT[j]--;
+        O[COUNT[j]] = A[i];
+    }
+}
+
+int main() {
+    generateRandoms();
+
+    countingSort();
+
+
+    return 0;
+}
