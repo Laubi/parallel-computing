29fbfb2031153fe23129e69e981b677eb6f4d35a
David Laubreiter <david.laubreiter@outlook.com>
2022-10-21 15:54:59 +0200

    fix(gauss): Fix algorithm

--- homework_1/gauss.c
+++ homework_1/gauss.c
@@ -3,38 +3,67 @@
 #include <stdlib.h>
 
 #ifndef MATRIX_SIZE
-#   define MATRIX_SIZE 1000
+#   define MATRIX_SIZE 10
 #endif
 
 int a[MATRIX_SIZE][MATRIX_SIZE];
+
+
+void swap_rows(int h, int max) {
+    for(int i = 0; i < MATRIX_SIZE; i++) {
+        int t = a[h][i];
+        a[h][i] = a[max][i];
+        a[max][i] = t;
+    }
+}
 
 void initializeWithRandoms() {
     srand(time(0));
 
     for (int i = 0; i < MATRIX_SIZE; i++) {
         for (int j = 0; j < MATRIX_SIZE; j++) {
-            a[i][j] = rand() % 10;
+            a[i][j] = rand();
         }
     }
 }
 
 void gauss() {
-    for(int k = 0; k < MATRIX_SIZE; k++) {
-        for(int i = 0; i < MATRIX_SIZE; i++) {
-            int pivot = a[i][k];
+    int h = 0;
+    int k = 0;
 
-            for(int j = 0; j < MATRIX_SIZE; j++) {
-                if (i != k) {
-                    int pivot_mult = pivot*a[k][j];
-                    int el_mult = a[k][k] * a[i][j];
-                    a[i][j] = el_mult - pivot_mult;
-                }
+    while(h < MATRIX_SIZE && k < MATRIX_SIZE) {
+        int i_max = INT_MIN;
+
+        for(int i = h; i < MATRIX_SIZE;   i++) {
+            if (i_max < a[i][k]) {
+                i_max = i;
             }
         }
+
+        if (a[i_max][k] ==0) {
+            k ++;
+            continue;
+        }
+
+        swap_rows(h, i_max);
+
+        for(int i = h+1; i < MATRIX_SIZE; i++) {
+            double f = (double) a[i][k] / a[h][k];
+
+            a[i][k] = 0;
+            for(int j = k+1; j < MATRIX_SIZE; j++) {
+                a[i][j] = a[i][j] - a[h][j] * f;
+            }
+        }
+        h++;
+        k++;
     }
 }
 
 int main() {
     initializeWithRandoms();
+
     gauss();
+
+    return 0;
 }
