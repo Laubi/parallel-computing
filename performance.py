5e2c07a6d17a6e8c6389dda2b5930a7de13a251b
David Laubreiter <david.laubreiter@dynatrace.com>
2023-01-05 17:49:14 +0100

    Fix perf script

--- performance.py
+++ performance.py
@@ -1,60 +1,80 @@
 #!/usr/bin/env python3
+
 import os
 import subprocess
 from pathlib import Path
 import csv
 
 # sort algos
-sort_len_sizes = [1_000, 2_500, 5_000, 10_000, 25_000, 50_000, 100_000, 250_000, 500_000, 1_000_000]
+sort_len_sizes = [
+    1_000,
+    2_500,
+    5_000,
+    10_000,
+    25_000,
+    50_000,
+    100_000,
+    250_000,
+    # 500_000,
+    # 1_000_000,
+    # 2_500_000,
+    # 5_000_000,
+    # 10_000_000,
+]
 
 # matrix algos
-matrix_sizes = [10, 100, 250, 500, 1_000, 2_500, 5_000, 10_000, 25_000]
+matrix_sizes = [
+    10,
+    100,
+    250,
+    500,
+    1_000,
+    2_500,
+    5_000,
+    # 10_000,
+    # 25_000,
+]
 
-# sieve
-sieve_sizes = [10_000, 100_000, 250_000, 500_000, 1_000_000, 10_000_000, 100_000_000]
+cores = [
+    1,
+    2,
+    4,
+    8,
+]
 
-configs = [
-    ("bubblesort", "bubblesort.c", ("ARR_LEN", sort_len_sizes)),
-    ("bucket", "bucket.c", ("ARR_LEN", sort_len_sizes)),
-    ("counting", "counting.c", ("ARR_LEN", sort_len_sizes)),
-    ("insert", "insert.c", ("ARR_LEN", sort_len_sizes)),
-    ("quicksort", "quicksort.c", ("ARR_LEN", sort_len_sizes)),
-    ("selection", "selection.c", ("ARR_LEN", sort_len_sizes)),
-    ("sieve", "sieve.c", ("ARR_LEN", sort_len_sizes)),
-    ("matrix", "matrix.c", ("MATRIX_SIZE", matrix_sizes)),
-    ("dijkstra", "dijkstra.c", ("MATRIX_SIZE", matrix_sizes)),
-    ("gauss", "gauss.c", ("MATRIX_SIZE", matrix_sizes))
+
+targets = [
+    ("matrix", matrix_sizes),
+    ("bucket", sort_len_sizes),
+    ("brick", sort_len_sizes),
+    ("counting", sort_len_sizes),
+    ("insert", sort_len_sizes),
+    ("selection", sort_len_sizes),
 ]
 
 Path("out").mkdir(exist_ok=True)
+
 with open("./out/times.cvs", "w+") as f:
     writer = csv.writer(f)
     writer.writerow(["Program", "Size", "Time"])
 
-    for config in configs:
-        k = config[0]
-        s = config[1]
-        p = config[2]
+    for config in targets:
+        target = config[0]
+        sizes = config[1]
 
-        variations = map(lambda v: (p[0], v), p[1])
+        for cur_cores in cores:
+            for size in sizes:
 
-        for variation in variations:
+                name = "{} - {} cores".format(target, cur_cores)
 
-            print("Starting {}: {}".format(k, variation[1]))
+                print("Starting {}. CORES: {} SIZE: {}".format(target, cur_cores, size))
 
-            source_file = "src/{}".format(s)
-            program_name = "out/{}".format(k)
+                env = dict(os.environ, SIZE=str(size), OMP_NUM_THREADS=str(cur_cores))
 
-            preprocessor_var = "-D{}={}".format(variation[0], variation[1])
+                make_target = "{}_omp".format(target)
+                out = subprocess.run(["make", "-s", make_target], env=env, stdout=subprocess.PIPE).stdout.decode("utf-8")
 
-            out = subprocess.run(["gcc", "-mcmodel=large", preprocessor_var, source_file, "-lm", "-o", program_name], stdout=subprocess.PIPE).stdout.decode("utf-8")
-            print(out)
+                print(out)
 
-            if os.environ.get("USE_SRUN") is None:
-                time = subprocess.run([program_name], stdout=subprocess.PIPE).stdout.decode("utf-8")
-            else:
-                time = subprocess.run(["srun", program_name], stdout=subprocess.PIPE).stdout.decode("utf-8")
-            print(time)
-
-            writer.writerow([k, variation[1], time])
+                writer.writerow([name, size, out])
 
