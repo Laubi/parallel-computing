043022a98e73fa8dcefd0e375cea10caa6e5f187
David Laubreiter <david.laubreiter@outlook.com>
2023-01-21 16:35:55 +0100

    sieve omp

--- src/sieve.c
+++ src/sieve.c
@@ -14,9 +14,14 @@ uint_fast8_t ARR[SIZE];
 void sieve() {
     size_t upper_bound = (size_t) sqrt(SIZE);
 
-    for (size_t i = 2; i < upper_bound; i++) {
+#pragma omp parallel for schedule(static)
+
+    for (size_t i = 2; i < upper_bound; i+=2) {
         if (ARR[i] == true) {
 
+           // size_t const upper =  (size_t) ceil((double)(SIZE - i * i) / i);
+           // #pragma omp parallel for schedule(dynamic)
+           // for (int j = 0; j < upper; j++) {
             for (int j = 0; i * i + j * i < SIZE; j++) {
                 ARR[i * i + j * i] = false;
             }
